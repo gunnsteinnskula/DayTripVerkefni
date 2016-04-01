4e7161c9b69dac5ca54a6101c2b93dd3012e538a
helgaj <helgajonasd@gmail.com>
2016-04-01 12:18:06 +0000

    Added files via upload

---
+++ look/Booking.java
@@ -0,0 +1,89 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class Booking extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTextField txtEmailAddress;
+	private JTextField txtCountry;
+	private JTextField txtAge;
+	private JTextField txtGroupSize;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Booking frame = new Booking();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Booking() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setText("Name...");
+		txtName.setBounds(26, 74, 193, 26);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		txtEmailAddress = new JTextField();
+		txtEmailAddress.setText("Email address...");
+		txtEmailAddress.setBounds(26, 112, 195, 26);
+		contentPane.add(txtEmailAddress);
+		txtEmailAddress.setColumns(10);
+		
+		txtCountry = new JTextField();
+		txtCountry.setText("Country...");
+		txtCountry.setBounds(26, 152, 196, 26);
+		contentPane.add(txtCountry);
+		txtCountry.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Bóka");
+		btnNewButton.setBackground(Color.ORANGE);
+		btnNewButton.setBounds(165, 227, 132, 29);
+		contentPane.add(btnNewButton);
+		
+		txtAge = new JTextField();
+		txtAge.setText("Age...");
+		txtAge.setBounds(297, 91, 99, 26);
+		contentPane.add(txtAge);
+		txtAge.setColumns(10);
+		
+		txtGroupSize = new JTextField();
+		txtGroupSize.setText("Group size...");
+		txtGroupSize.setBounds(297, 129, 99, 26);
+		contentPane.add(txtGroupSize);
+		txtGroupSize.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("    Please fill in the information down here for booking");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		lblNewLabel.setBounds(6, 16, 422, 16);
+		contentPane.add(lblNewLabel);
+	}
+}
---
+++ look/View.java
@@ -0,0 +1,94 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.DefaultListModel;
+import javax.swing.JButton;
+import javax.swing.JTable;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.UIManager;
+import java.awt.List;
+import javax.swing.JScrollPane;
+
+public class View {
+
+	private JFrame frmTripsInIceland;
+	private JTextField txtEnterTrip;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					View window = new View();
+					window.frmTripsInIceland.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public View() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frmTripsInIceland = new JFrame();
+		frmTripsInIceland.setTitle("Trips in Iceland");
+		frmTripsInIceland.getContentPane().setBackground(Color.LIGHT_GRAY);
+		frmTripsInIceland.getContentPane().setForeground(Color.BLACK);
+		frmTripsInIceland.setBounds(100, 100, 450, 300);
+		frmTripsInIceland.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmTripsInIceland.getContentPane().setLayout(null);
+		
+		txtEnterTrip = new JTextField();
+		txtEnterTrip.setText("Enter Trip...");
+		txtEnterTrip.setFont(new Font("Lucida Grande", Font.BOLD, 14));
+		txtEnterTrip.setBounds(6, 53, 300, 82);
+		frmTripsInIceland.getContentPane().add(txtEnterTrip);
+		txtEnterTrip.setColumns(1);
+		
+		JButton btnNewButton = new JButton("Search");
+		btnNewButton.setFont(new Font("Monaco", Font.BOLD, 14));
+		btnNewButton.setForeground(Color.BLACK);
+		btnNewButton.addActionListener(new ActionListener() {
+			//setja inn hérna tenginu við leitarorð í gagnagrunni, þegar ytt er á takk gerist þetta
+			public void actionPerformed(ActionEvent ars0) {
+				// a eða args0 þarna uppi
+				DefaultListModel DLM=new DefaultListModel();
+				DLM.addElement("Hestaferðir");
+				DLM.addElement("Gönguferðir");
+				DLM.addElement("Hestaferðir");
+				DLM.addElement("Hestaferðir");
+				
+				
+			}
+		});
+		btnNewButton.setBackground(Color.YELLOW);
+		btnNewButton.setBounds(318, 77, 117, 36);
+		frmTripsInIceland.getContentPane().add(btnNewButton);
+		
+		table = new JTable();
+		table.setBounds(352, 97, 1, 1);
+		frmTripsInIceland.getContentPane().add(table);
+		
+		List list = new List();
+		
+		
+		list.setBounds(91, 158, 138, 82);
+		frmTripsInIceland.getContentPane().add(list);
+	}
+}
